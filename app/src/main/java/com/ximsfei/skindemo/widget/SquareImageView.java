36aeed11ac170fef71b566708c4e28ebfa8aaa0c
pengfengwang <550616352@qq.com>
2017-01-16 00:27:28 +0800

    NavigationView 夜间模式, 首页item矩形
    Signed-off-by: pengfengwang <550616352@qq.com>

---
+++ app/src/main/java/com/ximsfei/skindemo/widget/SquareImageView.java
@@ -0,0 +1,29 @@
+package com.ximsfei.skindemo.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by pengfengwang on 2017/1/16.
+ */
+
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+    }
+}
